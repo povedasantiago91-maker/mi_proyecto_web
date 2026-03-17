e1c740d6264228199fa818249d725833bcfb54b1
povedasantiago91-market <povedasantiago91@gmail.com>
2026-03-16 20:07:43 -0500

    Corregir URL de conexión

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "mysql+pymysql://root:AXhUyEBkIhpPAGIOawRbgewyBakYiria@shortline.proxy.rlwy.net:53917/railway"
 
 engine = create_engine(DATABASE_URL)
 
